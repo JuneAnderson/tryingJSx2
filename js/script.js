0bf9467be3858de1ae14d021829988c6a55c7576
June <ktototam09@gmail.com>
2024-07-05 17:51:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict"; 
+/* 
+const result = confirm("Are you loh?"); 
+console.log(result);  */
+
+/* const answer = prompt("Are you pidor?", "yes I am"); 
+console.log(answer); */
+
+const answers = []; 
+answers[0] = prompt("Who is loh?", ""); 
+answers[1] = prompt("Who is pidor?", ""); 
+answers[2] = prompt("Who are you?", ""); 
+
+document.write(answers); 
